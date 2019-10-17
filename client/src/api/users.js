3c88cbda58188e857a2b3f8b7763b453d5299907
Rupert Barton <rbarton@scottlogic.com>
2019-10-17 10:23:02 +0100

    added loading spinner

--- client/src/api/users.js
+++ client/src/api/users.js
@@ -1,25 +1,37 @@
-import firebase, {functions} from './firebaseApp';
+import firebase, { functions } from './firebaseApp';
+import { updateMembers, updateExec, setExecLoading, setMembersLoading } from '../actions/users';
+
 
 let db = firebase.firestore();
 
-export const fetchMembers = () => {
+export const fetchMembers = () => dispatch => {
+  dispatch(setMembersLoading(true))
   return db.collection('users').where('isMember', '==', true).get().then(snapshot => {
     let members = [];
     snapshot.forEach(doc => {
       members.push({ ...doc.data(), id: doc.id });
     });
     return members;
-  });
+  })
+    .then(members => {
+      dispatch(updateMembers(members))
+      dispatch(setMembersLoading(false))
+    })
 };
 
-export const fetchExec = () => {
+export const fetchExec = () => dispatch => {
+  dispatch(setExecLoading(true))
   return db.collection('roles').get().then(snapshot => {
     let exec = [];
     snapshot.forEach(doc => {
       exec.push({ ...doc.data(), id: doc.id });
     });
     return exec;
-  });
+  })
+    .then(users => {
+      dispatch(updateExec(users))
+      dispatch(setExecLoading(false))
+    })
 };
 
 export const updateExecRoles = (changes) => {
--- client/src/components/content/modifyExec/modifyExecPageContainer.js
+++ client/src/components/content/modifyExec/modifyExecPageContainer.js
@@ -1,16 +1,17 @@
 import { connect } from 'react-redux';
 import ModifyExecPage from './ModifyExecPage';
-import { updateMembers, updateExec } from '../../../actions/users';
 import { fetchMembers, fetchExec } from '../../../api/users';
 
 const mapDispatchToProps = dispatch => ({
-  fetchMembers: () => fetchMembers().then(users => dispatch(updateMembers(users))),
-  fetchExec: () => fetchExec().then(users => dispatch(updateExec(users))),
+  fetchMembers: () => dispatch(fetchMembers()),
+  fetchExec: () => dispatch(fetchExec()),
 });
 
 const mapStateToProps = state => ({
   members: state.users.members,
-  execRoles: state.users.execRoles
+  execRoles: state.users.execRoles,
+  isExecLoading: state.users.isExecLoading,
+  isMembersLoading: state.users.isMembersLoading
 });
 
 export default connect(mapStateToProps, mapDispatchToProps)(ModifyExecPage);
